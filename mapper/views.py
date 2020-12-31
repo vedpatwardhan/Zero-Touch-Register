5ef7cbb5f6f5a91ce12f3c762730277c19318f6d
anupampatil44 <anupampatil44@gmail.com>
2020-12-31 23:57:09 +0530

    Exception Handling for checking if Reference ID has been issued completed

--- mapper/views.py
+++ mapper/views.py
@@ -12,11 +12,10 @@ import os
 from datetime import datetime
 from dateutil import tz
 from .models import visitor
-
-
+from django.contrib import messages
 path=os.getcwd()
 
-
+from django.shortcuts import redirect
 def home(request):
     return render(request,'Index.html')
 def entry(request):
@@ -45,9 +44,14 @@ def entry(request):
             other1=other
         print("Value of other: ",other1)
         image=path+"/image.jpg"
-        log=visitor(name=name,entry=current_time,phone=phone,address=address,other=other1,purpose=purpose,email=email,identity=identity,Reference=Reference,aadhar=aadhar,section=section,photo=image)
-        log.save()
-        return render(request, 'Index.html')
+        if(visitor.objects.filter(Reference=Reference).order_by('-id').exists()):
+            query=visitor.objects.filter(Reference=Reference).order_by('-id')[0]
+            if(query.exit=="Still in Campus"):
+                messages.error(request, 'Reference ID has already been issued.')
+        else:
+            log=visitor(name=name,entry=current_time,phone=phone,address=address,other=other1,purpose=purpose,email=email,identity=identity,Reference=Reference,aadhar=aadhar,section=section,photo=image)
+            log.save()
+            return render(request, 'Index.html')
     return render(request,'Entry_Form.html')
 
 def exit(request):
@@ -60,16 +64,15 @@ def exit(request):
             Reference_student = request.POST.get('Reference_student')
 
             o=visitor.objects.filter(Reference=Reference_student).order_by('-id')[0]
-            print(o)
             if(o.exit=="Still in Campus"):
                 o.exit=str(current_time)
                 o.save()
+                return render(request, 'Index.html')
             else:
-                return HttpResponse('<b>Reference no. not found as Entered</b>')
-            return render(request, 'Index.html')
+                messages.error(request, 'Reference ID is not issued.')
 
         except:
-            return HttpResponse('<b>Reference no. not found</b>')
+            messages.error(request, 'Reference ID not issued.')
 
     return render(request, 'Exit_form.html')
 
