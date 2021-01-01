1441cb1da5c80dca4c12419098834220fa1e1fd6
anupampatil44 <anupampatil44@gmail.com>
2021-01-01 13:23:27 +0530

    Final

--- mapper/views.py
+++ mapper/views.py
@@ -40,7 +40,7 @@ def entry(request):
         print("Value of other: ",other1)
         imagename1=""
         if (imagename == None):
-            print("HERE")
+
             imagename1 += "NA"
         else:
             imagename1=imagename
@@ -84,7 +84,7 @@ def export_users_csv_today(request):
     response['Content-Disposition'] = 'attachment; filename="Today\'s Report.csv"'
     # today=str(datetime.date.today())
     writer = csv.writer(response)
-    writer.writerow(['Sr No.','Name','Entry Time','Entry Date', 'Exit Time', 'Phone', 'Email', 'Address', 'Purpose', 'Identity', 'If Other then Specify', 'Reference ID','Aadhar','Section to be Visited ','Image Name As Taken'])
+    writer.writerow(['Sr No.','Name','Entry Time','Entry Date', 'Exit Time', 'Phone', 'Email', 'Address', 'Purpose', 'Identity', 'If Other then Specify', 'Reference ID','Aadhar','Section to be Visited ','Image Name As Taken on Device'])
 
     users = visitor.objects.filter(dateofentry=str(date.today())).values_list()
     for user in users:
@@ -97,7 +97,7 @@ def export_users_csv_overall(request):
     response['Content-Disposition'] = 'attachment; filename="Overall Report.csv"'
     # today=str(datetime.date.today())
     writer = csv.writer(response)
-    writer.writerow(['Sr No.','Name','Entry Time','Entry Date', 'Exit Time', 'Phone', 'Email', 'Address', 'Purpose', 'Identity', 'If Other then Specify', 'Reference ID','Aadhar','Section to be Visited ','Image Name As Taken'])
+    writer.writerow(['Sr No.','Name','Entry Time','Entry Date', 'Exit Time', 'Phone', 'Email', 'Address', 'Purpose', 'Identity', 'If Other then Specify', 'Reference ID','Aadhar','Section to be Visited ','Image Name As Taken','Image Name As Taken on Device'])
 
     users = visitor.objects.all().values_list()
     for user in users:
@@ -110,7 +110,7 @@ def export_users_csv_inside(request):
     response['Content-Disposition'] = 'attachment; filename="Still In Campus Report.csv"'
     # today=str(datetime.date.today())
     writer = csv.writer(response)
-    writer.writerow(['Sr No.','Name','Entry Time','Entry Date', 'Exit Time', 'Phone', 'Email', 'Address', 'Purpose', 'Identity', 'If Other then Specify', 'Reference ID','Aadhar','Section to be Visited ','Image Name As Taken'])
+    writer.writerow(['Sr No.','Name','Entry Time','Entry Date', 'Exit Time', 'Phone', 'Email', 'Address', 'Purpose', 'Identity', 'If Other then Specify', 'Reference ID','Aadhar','Section to be Visited ','Image Name As Taken','Image Name As Taken on Device'])
 
     users = visitor.objects.filter(exit="Still in Campus").values_list()
     for user in users:
